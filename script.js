a093b556d561b22fcaad05926eb4209f1b6fb141
Dbarbosadesigner <dmbarbosa1991@gmail.com>
2024-07-04 17:02:49 -0300

    Adicionado menu barra lateral

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-function menu(params) {
-        var menu=document.getElementById('menuVert');
-        if (menu.style.display === "none") {
-            menu.style.display="block"
-        }
-        else{
-            menu.style.display="none"
-        }
-}
 
