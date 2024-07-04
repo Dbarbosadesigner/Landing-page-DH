38930a682bc0a0a9027d56fbbcd2becb9959dc85
Dbarbosadesigner <dmbarbosa1991@gmail.com>
2024-07-04 19:00:09 -0300

    Estilizado menu barra lateral

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+var btn = document.querySelector('.ocultar');
+var container = document.querySelector('.barra');
+
+btn.addEventListener('click', function(){
+    if (container.style.display === 'block') {
+        container.style.display = 'none'
+    }
+    else{
+        container.style.display = 'block'
+    }
+})
+
 
